79d8c2196acbe48695b1f201cb6cd6cb3270f67e
Rick Perez <Rickperez87@gmail.com>
2021-02-10 21:36:33 -0500

    initialization

---
+++ index.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+  const title = todo.title;
+  const finished = todo.completed;
+  console.log(todo, title, finished);
+});
